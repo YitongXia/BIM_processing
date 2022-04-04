48cdcbf075ebb07bf3caac45816152ed86acb5c1
Yitong Xia <xiayitong0630@gmail.com>
2022-04-04 18:55:12 +0200

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -28,8 +28,6 @@ struct Polyhedron_builder : public CGAL::Modifier_base<HDS> {
     Polyhedron_builder() {}
     void operator()(HDS& hds) {
         CGAL::Polyhedron_incremental_builder_3<HDS> builder(hds, true);
-        std::cout << "constructing polyhedron -- ";
-        std::cout << "building surface with " << vertices.size() << " vertices and " << faces.size() << " faces" << '\n';
 
         builder.begin_surface(vertices.size(), faces.size());
         for (auto const& vertex : vertices) builder.add_vertex(vertex);
@@ -62,7 +60,7 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
 /*
  * this function is to write test output file, as obj format
  */
-static void output_OBJ_file(const std::vector<Point>& vertices, const std::vector<std::vector<unsigned long>> faces,std::string& fname) {
+static void output_OBJ_file(const std::vector<Point>& vertices, const std::vector<std::vector<unsigned long>> &faces,std::string& fname) {
 
     std::ofstream output_file;
 
@@ -75,6 +73,29 @@ static void output_OBJ_file(const std::vector<Point>& vertices, const std::vecto
     for(const auto &face:faces)
     {
         output_file<<"f "<<face[0]<<" "<<face[1]<<" "<<face[2]<<std::endl;
+    }
+    // close file
+    output_file.close();
+}
+
+ void output_txt_file(const std::vector<Point>& vertices, const std::vector<std::vector<unsigned long>>& faces,std::string& fname) {
+
+    std::ofstream output_file;
+
+    output_file.open(fname, std::ios::out | std::ios::trunc);
+    output_file << std::fixed;
+
+    // print data into the file
+    for(const auto& ver:vertices)
+        output_file<<"v " << ver.x()<<" "<< ver.y()<<";" << ver.z()<<std::endl;
+
+    for(const auto &face:faces)
+    {
+        output_file<<"f ";
+        for(const auto &v:face){
+            output_file<<v<<" ";
+        }
+        output_file<<'\n';
     }
     // close file
     output_file.close();
@@ -144,13 +165,18 @@ std::vector<Polyhedron> read_ifc(std::string& fname) {
                         bool mark= false;
                         for(unsigned long j=0;j<unique_vertices.size();++j)
                         {
-                            if(i != j)
-                            {
-                                if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
+                            if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
                                 {
                                     mark= true;
                                 }
-                            }
+
+//                            if(i != j)
+//                            {
+//                                if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
+//                                {
+//                                    mark= true;
+//                                }
+//                            }
                         }
                         if(!mark) {
                             unique_vertices.emplace_back(cur_vertices[i]);
@@ -176,16 +202,16 @@ std::vector<Polyhedron> read_ifc(std::string& fname) {
 
        // ----use DT to create convex hull -> create polyhedron
                 Delaunay T;
+                std::string fname2="delaunay.obj";
+                output_txt_file(cur_vertices,face_v_indices,fname2);
                 T.insert(cur_vertices.begin(), cur_vertices.end());
                 Polyhedron polyhedron;
                 CGAL::convex_hull_3_to_face_graph(T, polyhedron);
                 polyhedrons.emplace_back(polyhedron);
-                std::cout << "build polyhedron now" << '\n';
-                std::cout << "done" << '\n';
                 face_v_indices.clear();
                 cur_vertices.clear();
 
-                polyhedron.delegate(polyhedron_builder);
+                //polyhedron.delegate(polyhedron_builder);
 
 //       ----use polyhedron_builder to construct polyhedron
 //                for (auto &coordinates: unique_vertices) {
@@ -220,20 +246,24 @@ std::vector<Polyhedron> read_ifc(std::string& fname) {
         // this function is to remove redundant vertices
         for(unsigned long i=0 ; i<cur_vertices.size() ; ++i)
         {
-            std::cout<<"current vertices"<<cur_vertices[i].x()<<", "<<cur_vertices[i].y()<<", "<<cur_vertices[i].z()<<std::endl;
+            //std::cout<<"current vertices"<<cur_vertices[i].x()<<", "<<cur_vertices[i].y()<<", "<<cur_vertices[i].z()<<std::endl;
             if(unique_vertices.empty()) unique_vertices.emplace_back(cur_vertices[i]);
             else
             {
                 bool mark= false;
                 for(unsigned long j=0;j<unique_vertices.size();++j)
                 {
-                    if(i != j)
+                    if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
                     {
-                        if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
-                        {
-                            mark= true;
-                        }
+                        mark= true;
                     }
+//                    if(i != j)
+//                    {
+//                        if(cur_vertices[i].x() == unique_vertices[j].x()&&cur_vertices[i].y() == unique_vertices[j].y() && cur_vertices[i].z() == unique_vertices[j].z() )
+//                        {
+//                            mark= true;
+//                        }
+//                    }
                 }
                 if(!mark) {
                     unique_vertices.emplace_back(cur_vertices[i]);
@@ -263,9 +293,7 @@ std::vector<Polyhedron> read_ifc(std::string& fname) {
         CGAL::convex_hull_3_to_face_graph(T, polyhedron);
         polyhedrons.emplace_back(polyhedron);
 
-        std::cout << "build polyhedron now" << '\n';
 
-        std::cout << "done" << '\n';
         face_v_indices.clear();
         cur_vertices.clear();
 
@@ -407,10 +435,23 @@ void output_OFF_file(const Nef_polyhedron& nef_p,const std::string& f) {
  * this is shell explorer
  */
 
+bool vertex_exist_check(std::vector<Point>& vertices, Point& vertex) {
+    bool flag= false;
+    for (auto& v : vertices) {
+        if (CGAL::to_double(v.x()) == CGAL::to_double(vertex.x()) &&
+            CGAL::to_double(v.y()) == CGAL::to_double(vertex.y()) &&
+        CGAL::to_double(v.z()) == CGAL::to_double(vertex.z()))
+        {
+            flag= true;
+        }
+    }
+    return flag;
+}
 
 struct Shell_explorer {
     std::vector<Point> vertices;
     std::vector<std::vector<unsigned long>> faces;
+    unsigned long p_index=1;
 
     void visit(Nef_polyhedron::Vertex_const_handle v) {}
     void visit(Nef_polyhedron::Halfedge_const_handle he) {}
@@ -418,9 +459,10 @@ struct Shell_explorer {
     void visit(Nef_polyhedron::SHalfloop_const_handle shl) {}
     void visit(Nef_polyhedron::SFace_const_handle sf) { }
 
+
     void visit(Nef_polyhedron::Halffacet_const_handle hf) {
 
-        unsigned long p_index=1;
+
         for (Nef_polyhedron::Halffacet_cycle_const_iterator it = hf->facet_cycles_begin(); it != hf->facet_cycles_end(); it++) {
 
             //std::cout << it.is_shalfedge() << " " << it.is_shalfloop() << '\n';
@@ -438,31 +480,66 @@ struct Shell_explorer {
             {
                 Nef_polyhedron::SVertex_const_handle svert = hc_start->source();
                 Point vpoint = svert->center_vertex()->point();
-                std::cout << "v: " << "(" << vpoint.x() << ", " << vpoint.y() << ", " << vpoint.z() << ")" << '\n';
-                vertices.push_back(vpoint); // add vertex to se's vertices
+                vertices.emplace_back(vpoint);
                 f.emplace_back(p_index);
                 p_index++;
             }
             faces.emplace_back(f);
-            std::cout << '\n';
         }
     }
 };
 
-class MyPoint {
-    double x;
-    double y;
-    double z;
-    MyPoint(double x1, double y1,double z1)
+class Jshell{
+public:
+    std::vector<Point> vertices;
+    std::vector<std::vector<unsigned long>> faces;
+    //std::vector<std::string> semantics; // semantic for each face in jshells[0] - exterior shell
+
+    Jshell()
+    {}
+    Jshell(std::vector<Point> v1,std::vector<std::vector<unsigned long>> f1)
     {
-        x=x1;
-        y=y1;
-        z=z1;
+        vertices=v1;
+        faces=f1;
     }
 };
 
-void write_cityjson(const Nef_polyhedron& big_nef) {
+std::vector<Jshell> store_shell(const Nef_polyhedron& big_nef) {
 
+    std::vector<Jshell> shell_list;
+
+    std::vector<Point> vertices;
+    std::vector<std::vector<unsigned long>> faces;
+
+    int volume_count = 0;
+    Nef_polyhedron::Volume_const_iterator current_volume;
+
+    CGAL_forall_volumes(current_volume, big_nef) {
+
+        std::cout << "volume: " << volume_count++ << " ";
+        std::cout << "volume mark: " << current_volume->mark() << '\n';
+
+        Nef_polyhedron::Shell_entry_const_iterator current_shell;
+
+        CGAL_forall_shells_of(current_shell, current_volume) { // iterate each shell
+
+            if(volume_count == 5)
+            {
+                Shell_explorer se;
+                Nef_polyhedron::SFace_const_handle sface_in_shell(current_shell);
+                big_nef.visit_shell_objects(sface_in_shell, se);
+
+                Jshell jshell=Jshell(se.vertices,se.faces);
+                shell_list.emplace_back(jshell);
+            }
+        }
+    }
+    return shell_list;
+}
+
+void write_cityjson(const std::vector<Jshell>& jshell)
+{
+    int volume_count = 0;
     nlohmann::json j;
     j["type"] = "CityJSON";
     j["version"] = "1.0";
@@ -470,138 +547,115 @@ void write_cityjson(const Nef_polyhedron& big_nef) {
     j["transform"]["scale"] = json::array({1.0, 1.0, 1.0});
     j["transform"]["translate"] = json::array({0.0, 0.0, 0.0});
     j["CityObjects"] = json::object();
-    j["CityObjects"]["Building_1"]=json::object();
+    j["CityObjects"]["Building-1"]=json::object();
 
-    j["CityObjects"]["Building_1"]["attributes"]=json::object();
-    j["CityObjects"]["Building_1"]["children"]=json::array();
+    j["CityObjects"]["Building-1"]["attributes"]=json::object();
+    j["CityObjects"]["Building-1"]["children"]=json::array({"Building-1-0"});
+
     // ----test-----
-    j["CityObjects"]["Building_1"]["geometry"] = json::array();
-    j["CityObjects"]["Building_1"]["type"]="Building";
+    j["CityObjects"]["Building-1"]["geometry"] = json::array();
+    j["CityObjects"]["Building-1"]["type"]="Building";
 
     std::vector<Point> all_vertices;
-    int volume_count = 0;
-    int shell_count = 0;
-    Nef_polyhedron::Volume_const_iterator current_volume;
-    CGAL_forall_volumes(current_volume, big_nef) {
-
-        std::cout << "volume: " << volume_count++ << " ";
-        std::cout << "volume mark: " << current_volume->mark() << '\n';
+    for(auto & se:jshell)
+    {
         std::string building_name="Building-1-"+ std::to_string(volume_count);
         j["CityObjects"][building_name]=json::object();
         j["CityObjects"][building_name]["attributes"]=json::object();
         j["CityObjects"][building_name]["geometry"] = json::array();
 
+        std::vector<Point> unique_vertices;
+        std::vector<Point> cur_vertices = se.vertices;
+        std::vector<std::vector<unsigned long>> face_v_indices=se.faces;
 
-        Nef_polyhedron::Shell_entry_const_iterator current_shell;
-        bool shell_mark= false;
+        if(all_vertices.empty())
+            all_vertices=se.vertices;
 
-        CGAL_forall_shells_of(current_shell, current_volume) { // iterate each shell
+        else{
+            std::vector<Point> temp=se.vertices;
+            for(auto & vertex: temp)
+                all_vertices.emplace_back(vertex);
+        }
 
-            Shell_explorer se;
-            Nef_polyhedron::SFace_const_handle sface_in_shell(current_shell);
-            big_nef.visit_shell_objects(sface_in_shell, se);
-            std::vector<Point> unique_vertices;
-
-            if(all_vertices.empty()) all_vertices = se.vertices;
-            else{
-                for(auto& vertex: se.vertices)
-                    all_vertices.emplace_back(vertex);
-            }
-
-            std::vector<Point>cur_vertices=se.vertices;
-            std::vector<std::vector<unsigned long>> face_v_indices=se.faces;
-
-            for(unsigned long i=0;i<all_vertices.size();++i)
+        for(auto & all_vertice : all_vertices)
+        {
+            if (unique_vertices.empty()) unique_vertices.emplace_back(all_vertice);
+            else
             {
-                if (unique_vertices.empty()) unique_vertices.emplace_back();
-                else
+                bool mark= false;
+
+                for(auto & unique_vertice : unique_vertices)
                 {
-                    bool mark= false;
-                    for(unsigned long k=0;k<unique_vertices.size();++k)
+                    if(CGAL::to_double(all_vertice.x()) == CGAL::to_double(unique_vertice.x())&&CGAL::to_double(all_vertice.y()) == CGAL::to_double(unique_vertice.y()) && CGAL::to_double(all_vertice.z()) == CGAL::to_double(unique_vertice.z()) )
                     {
-                        if(i != k)
-                        {
-                            if(all_vertices[i].x() == unique_vertices[k].x()&&all_vertices[i].y() == unique_vertices[k].y() && all_vertices[i].z() == unique_vertices[k].z() )
-                            {
-                                mark= true;
-                            }
-                        }
-                    }
-                    if(!mark) {
-                        unique_vertices.emplace_back(all_vertices[i]);
+                        mark= true;
                     }
                 }
-            }
-            for(auto & face:face_v_indices)
-            {
-                for(auto & vertex: face)
-                {
-
-                    for(unsigned long i = 0; i < unique_vertices.size(); ++i){
-                        if(unique_vertices[i].x()==all_vertices[vertex-1].x() && unique_vertices[i].y()==all_vertices[vertex-1].y() && unique_vertices[i].z()==all_vertices[vertex-1].z())
-                        {
-                            vertex=i;
-                            break;
-                        }
-                    }
+                if(!mark) {
+                    unique_vertices.emplace_back(all_vertice);
                 }
-            }
-            if(!shell_mark)
-            {
-                auto all_f=json::array();
-                for(auto & face:face_v_indices)
-                {
-                    auto single_f=json::array();
-                    for(auto & vertex: face){
-                        single_f.emplace_back(vertex);
-                    }
-                    auto shell_f=json::array();
-                    shell_f.emplace_back(single_f);
-                    all_f.emplace_back(shell_f);
-                }
-                auto geometry_contect=json::object();
-                geometry_contect["boundaries"]=json::array();
-                geometry_contect["boundaries"].emplace_back(all_f);
-                geometry_contect["lod"]="1.1";
-                geometry_contect["semantics"]=json::object();
-                geometry_contect["semantics"]["surfaces"]=json::array();
-                geometry_contect["semantics"]["values"]=json::array();
-                geometry_contect["type"]="Solid";
-
-                j["CityObjects"][building_name]["geometry"].emplace_back(geometry_contect);
-
-                // ----- currently there is no information about the surface
-
-                j["CityObjects"][building_name]["parents"]=json::array({"Building-1"});
-                j["CityObjects"][building_name]["type"]="BuildingRoom";
-
-                j["vertices"]=json::array();
-
-                for(const auto& vpoint:all_vertices)
-                {
-                    auto single_list=json::array({CGAL::to_double(vpoint.x()),CGAL::to_double(vpoint.y()),CGAL::to_double(vpoint.z())});
-                    j["vertices"].emplace_back(single_list);
-                }
-
-
-                shell_mark= true;
             }
         }
 
-    }
+        for(auto & face:face_v_indices)
+        {
+            for(auto & vertex: face)
+            {
+                for(unsigned long i = 0; i < unique_vertices.size(); ++i)
+                {
+                    if(CGAL::to_double(unique_vertices[i].x())==CGAL::to_double(cur_vertices[vertex-1].x()) && CGAL::to_double(unique_vertices[i].y())==CGAL::to_double(cur_vertices[vertex-1].y()) && CGAL::to_double(unique_vertices[i].z())==CGAL::to_double(cur_vertices[vertex-1].z()))
+                    {
+                        vertex=i+1;
+                        break;
+                    }
+                }
+            }
+        }
 
+        auto all_f=json::array();
+        for(auto & face:face_v_indices)
+        {
+            auto single_f=json::array();
+            for(auto & vertex: face){
+                single_f.emplace_back(vertex);
+            }
+            auto shell_f=json::array();
+            shell_f.emplace_back(single_f);
+            all_f.emplace_back(shell_f);
+        }
+        auto geometry_contect=json::object();
+        geometry_contect["boundaries"]=json::array();
+        geometry_contect["boundaries"].emplace_back(all_f);
+        geometry_contect["lod"]="2.2";
+        geometry_contect["type"]="Solid";
+
+        j["CityObjects"][building_name]["geometry"].emplace_back(geometry_contect);
+
+        // ----- currently there is no information about the surface
+
+        j["CityObjects"][building_name]["parents"]=json::array({"Building-1"});
+        j["CityObjects"][building_name]["type"]="BuildingRoom";
+
+        j["vertices"]=json::array();
+
+        for(const auto& vpoint:unique_vertices)
+        {
+            auto single_list=json::array({CGAL::to_double(vpoint.x()),CGAL::to_double(vpoint.y()),CGAL::to_double(vpoint.z())});
+            j["vertices"].emplace_back(single_list);
+        }
+        volume_count++;
+    }
 
     std::string json_string = j.dump(2);
     std::ofstream out_stream("mybuilding.json");
     out_stream << json_string;
     out_stream.close();
-
 }
 
 
-int main()
+ int main()
 {
-    std::string fname = "simple.obj";
+    std::string fname = "KIT.obj";
     std::vector<Polyhedron> polyhedrons_list = read_ifc(fname);
     Nef_polyhedron big_nef;
     for(auto & polyhedron:polyhedrons_list)
@@ -614,8 +668,9 @@ int main()
         std::cout<<"big nef is simple"<<std::endl;
     else std::cout<<"big nef is not simple"<<std::endl;
 
-    write_cityjson(big_nef);
-    
-    
+
+    std::vector<Jshell> shell_list = store_shell(big_nef);
+    write_cityjson(shell_list);
+
     return 0;
 }
